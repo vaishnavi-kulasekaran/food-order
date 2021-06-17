5ac26162b5dd4a1944402e0dd28f1e2ca624f755
vaishnavi-kulasekaran <72563225+vaishnavi-kulasekaran@users.noreply.github.com>
2021-06-17 09:30:27 +0530

    Delete partials directory

--- partials/footer.php
+++
@@ -1,9 +0,0 @@
-<!--Footer Section Starts-->
-       <div class="Footer">
-           <div class="wrapper">
-             <p class="text-center">2020 All rights reserved, Food House. Developed By - <a href="#">Vaishnavi</a></p>
-          </div>
-       </div>
-       <!--Footer Section Ends-->
-   </body>
-</html>
